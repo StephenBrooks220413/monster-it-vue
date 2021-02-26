6aa4b1c5264323608718dae3ca6dd8efe2bbfd04
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-02-26 12:36:30 -0800

    update

---
+++ src/views/Contact.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="contact">
+    <h1>Contact the Monster</h1>
+    <br /><br />
+    <div class="container">
+      hello
+    </div>
+  </div>
+</template>
+
+<script>
+// export default {
+// }
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  margin-top: 110px;
+
+  h1 {
+    text-shadow: 11px 10px 14px grey !important;
+    font-size: 3em;
+  }
+  .line {
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+.monster1 {
+  max-width: 300px;
+}
+a {
+  margin-left: 40px;
+  margin-right: 40px;
+  margin-bottom: 30px;
+}
+input {
+  background-color: rgb(168, 168, 168) !important;
+  opacity: 0.2 !important;
+}
+textarea {
+  background-color: rgb(168, 168, 168) !important;
+  opacity: 0.2 !important;
+}
+</style>
